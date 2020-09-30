9e08988e2e4699e8b64e925c172560960c143e02
KauanRakoski <kauan.k.rakoski@gmail.com>
2020-09-30 20:28:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const Reactor = (dataObj) => {
+    let signals = {}
+    
+    const observe = (property, signalHandler) => {
+        if (!signals.property) signals[property] = []
+        signals[property].push(signalHandler)
+    }
+
+    const notify = (signal, newValue) => {
+        if (!signals[signal] || signals[signal].length < 1) return
+
+        signals[signal].forEach((signalHandler) => signalHandler())
+    }
+    const makeReactive = (obj, key) => {
+        let val = obj[key]
+
+        Object.defineProperty(obj, key, {
+            get() {
+                return val
+            },
+            set(newValue) {
+                val = newValue
+                notify(key)
+            }
+        })
+    }
+
+    const observeData = (obj) => {
+        for (let key in obj) {
+            if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                makeReactive(obj, key)
+            }
+        }
+    }
+
+    observeData(dataObj)
+
+    return {
+        data: dataObj,
+        observe,
+        notify
+    }
+}
+
+let Character = Reactor(
+    {
+        name: "Jon",
+        lastname: "Snow"
+    }
+)
+
+Character.observe("name", () => console.log(Character.data.name))
+
+Character.data.name = "Ygritte"
