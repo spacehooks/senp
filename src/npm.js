52d5f72b22f3a6d495a5cf7610e814c469c21513
KauanRakoski <kauan.k.rakoski@gmail.com>
2020-12-09 21:29:54 -0300

    npm version

---
+++ src/npm.js
@@ -0,0 +1,50 @@
+exports.Trigger = (dataObj) => {
+    let signals = {}
+    
+    const observe = (property, signalHandler) => {
+        if (!signals.property) signals[property] = []
+        signals[property].push(signalHandler)
+    }
+
+    const notify = (signal) => {
+        if (!signals[signal] || signals[signal].length < 1) return
+
+        signals[signal].forEach((signalHandler) => signalHandler())
+    }
+
+    const makeReactive = (obj, key) => {
+        let val = obj[key]
+
+        Object.defineProperty(obj, key, {
+            get() {
+                return val
+            },
+            set(newValue) {
+                val = newValue
+                notify(key)
+            }
+        })
+    }
+
+    const observeData = (obj) => {
+        for (let key in obj) {
+            if (Object.prototype.hasOwnProperty.call(obj, key)) {
+                makeReactive(obj, key)
+            }
+        }
+    }
+
+    observeData(dataObj)
+
+    return {
+        data: dataObj,
+        observe,
+        notify
+    }
+}
+
+exports.WatchNode = (node, obj, property) => {
+    node.textContent = obj.data[property]
+  
+    obj.observe(property, value => node.textContent = obj.data[property] || '')
+  }
